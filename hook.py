f1f4ff636050e6de6c7aa8fe4db980b418809c36
Mathieu Dähne <mathieud@theodo.co.uk>
2017-09-17 13:22:25 +0100

    Sample app

--- hook.py
+++ hook.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 app = Flask(__name__)
 
+COLORS = {
+    'success': 24173,
+    'failed': 65423,
+}
+
 @app.route('/', methods=['GET', 'POST'])
-def hook(request):
-    print(request.data)
-    return "Hello World!"
+def hook():
+    payload = request.get_json().get('payload', {})
+    status = payload.get('status')
+    failed = payload.get('failed')
+    author = payload.get('author_email')
+
+    requests.put('http://theodobridge.local:3000/hue/{}'.format(COLORS.get(status)))
+
+    return "OK"
